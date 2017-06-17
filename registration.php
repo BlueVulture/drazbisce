7cc395bf44208df0476f716a65dc64b9ab7d33ab
RattleTotem <domen.ramsak@gmail.com>
2017-06-17 11:57:51 +0200

    registration

--- registration.php
+++ registration.php
@@ -4,16 +4,17 @@
 
 <h1>Registracija</h1>
 
-<form action="user_insert.php" method="post">
+<form action="user_new.php" method="post">
     <span class="user_input">
-    Ime: <input type="text" name="first_name" /><br />
-    Priimek: <input type="text" name="last_name" /><br />
-    E-pošto: <input type="email" name="email" /><br />
-    Telefon: <input type="text" name="phone" /><br />
-    Geslo: <input type="password" name="pass1"/><br />
-    Geslo(2x): <input type="password" name="pass2" /><br />
-    <input type="submit" name="submit" value="Shrani" />
-    <input type="reset" name="reset" value="Prekliči" />
+    Uporabniško ime: <input type="text" name="username"><br>
+    Ime: <input type="text" name="first_name" /><br>
+    Priimek: <input type="text" name="last_name" /><br>
+    E-pošto: <input type="email" name="email"><br>
+    Telefon: <input type="text" name="phone" ><br>
+    Geslo: <input type="password" name="pass1" required><br>
+    Ponovi geslo: <input type="password" name="pass2" required><br>
+    <input type="submit" name="submit" value="Pošlji">
+    <input type="reset" name="reset" value="Prekliči">
     </span>
 </form>
 

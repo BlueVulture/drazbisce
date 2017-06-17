3ec68da0fbae1c197fb2ad40cd65757ebbbd30e5
RattleTotem <domen.ramsak@gmail.com>
2017-06-17 18:08:59 +0200

    tables

--- index.php
+++ index.php
@@ -3,14 +3,16 @@
     require 'database.php'
 ?>
 <div id="content">
-  <div class="data" id="new-ads">
-    <p>Najnovejše dražbe</p>
-    <hr>
-  </div>
-  <div class="data" id="special-ads">
-    <p>Kmalu konec...</p>
-    <hr>
-  </div>
+  <table class="table" border="1">
+    <tr>
+      <td class="data" id="new-ads" rowspan="2"><p>Najnovejše dražbe</p></td>
+      <td class="data" id="special-ads"><p>Kmalu konec...</p></td>
+    </tr>
+    <tr >
+      <td></td>
+      <td></td>
+    </tr>
+  </table>
 </div>
 
 <?php

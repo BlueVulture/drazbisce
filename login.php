12ac1c20056b8107efe08aac170d702159b31405
RattleTotem <domen.ramsak@gmail.com>
2017-06-17 20:45:33 +0200

    login

--- login.php
+++ login.php
@@ -2,22 +2,22 @@
   require 'header.php';
  ?>
 
-<head>
-  	<title>Login</title>
-</head>
 
- <body>
-         <div class="login">
-             <h1>Prijava</h1>
-             <form method="post" action="login_action.php">
-                 <input type="text" name="user" placeholder="Uporabnik" required>
-                 <input type="password" name="pass" placeholder="Geslo" required>
-                 <input type="submit" value="Login">
-             </form>
+ <div class="data">
+     <h1>Prijava</h1>
+     <span id="login">
+     <form method="post" action="login_action.php">
+         <input type="text" name="user" placeholder="Uporabnik" required>
+         <br>
+         <input type="password" name="pass" placeholder="Geslo" required>
+         <br>
+         <button type="submit">Prijava</button>
+     </form>
+    </span>
 
-             <span><a href="registration.php">Registracija</a>
-         </div>
-     </body>
+     <span><a href="registration.php">Registracija</a>
+ </div>
+
 
  <?php
    include_once 'footer.php'
--- registration.php
+++ registration.php
@@ -1,24 +1,23 @@
 <?php
   include_once 'header.php';
  ?>
-<head>
-  <title>Registration</title>
-</head>
 
-<h1>Registracija</h1>
+<div class="data">
 
-<form action="user_new.php" method="post">
-    <span class="user_input">
-    Uporabniško ime: <input type="text" name="username"><br>
-    Ime: <input type="text" name="first_name"><br>
-    Priimek: <input type="text" name="last_name"><br>
-    E-pošto: <input type="email" name="email" required><br>
-    Geslo: <input type="password" name="pass1" required><br>
-    Ponovi geslo: <input type="password" name="pass2" required><br>
-    <input type="submit" name="submit" value="Pošlji">
-    <input type="reset" name="reset" value="Prekliči">
-    </span>
-</form>
+  <h1>Registracija</h1>
+
+  <table>
+    <form action="user_new.php" method="post">
+          <tr><td>Uporabniško ime:</td><td><input type="text" name="username"></td></tr>
+          <tr><td>Ime:</td><td><input type="text" name="first_name"></td></tr>
+          <tr><td>Priimek:</td><td> <input type="text" name="last_name"></td></tr>
+          <tr><td>E-pošta:</td><td><input type="email" name="email" required></td></tr>
+          <tr><td>Geslo:</td><td><input type="password" name="pass1" required></td></tr>
+          <tr><td>Ponovi geslo:</td><td><input type="password" name="pass2" required></td></tr>
+          <tr><td><button type="reset" name="reset">Prekliči</button></td><td><button type="submit" name="submit">Pošlji</button></td></tr>
+    </form>
+  </table>
+</div>
 
 <?php
   include_once 'footer.php'

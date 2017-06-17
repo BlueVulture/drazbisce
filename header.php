58e45208db5ce3f30a5d76012fd253467271b0dc
RattleTotem <domen.ramsak@gmail.com>
2017-06-17 11:22:23 +0200

    pages

--- header.php
+++ header.php
@@ -4,16 +4,8 @@
         <link rel="stylesheet" type="text/css" href="styles/main_style.css">
         <title>Dražbišče</title>
     </head>
-    
+
     <body>
         <header>
-            <div id="nav-top">
-                <div id="nav-top-wrapper">
-                    <ul>
-                        <li><a href="#">Domov</a></li>
-                        <li><a href="#">Najnoveša ponudba</a></li>
-                        <li><a href="#">Pomoč</a></li>
-                    </ul>
-                </div>
-            </div>      
+                
         </header>
